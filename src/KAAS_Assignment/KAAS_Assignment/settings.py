0e20eb3c0d685a91ec2332d24be0b825a27966f6
Aaron Gangemi <aaron.gangers@gmail.com>
2020-09-06 21:47:14 +0800

    Front end page added

--- src/KAAS_Assignment/KAAS_Assignment/settings.py
+++ src/KAAS_Assignment/KAAS_Assignment/settings.py
@@ -11,10 +11,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
+SETTINGS_PATH = os.path.dirname(os.path.dirname(__file__))
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'KAAS_Assignment.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(SETTINGS_PATH, 'ReDoS/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
