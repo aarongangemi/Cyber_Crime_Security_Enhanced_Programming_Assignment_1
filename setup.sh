ba12978bb9a41567b3d0ada9a6800b5e483ca895
student <yapkaymen@gmail.com>
2020-09-13 03:04:00 +0800

    Added a setup.sh for initial setup, vuln_requirements.txt for vulnerable app, updated src folder to work with django 2.2.3

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt-get update
+sudo apt-get install python3.5 python3-pip python3-virtualenv
+virtualenv -p /usr/bin/python3.5 ccsep-env
+source ccsep-env/bin/activate
+pip install -r vuln_requirements.txt
+deactivate
+echo "Activate virtualenv by typing \'source ccsep-env/bin/activate'"
+echo "Deactivate the virtualenv by typing 'deactivate'"
--- src/KAAS_Assignment/ReDoS/views.py
+++ src/KAAS_Assignment/ReDoS/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from django.views import View
 
-
+# Create your views here.
 class RedosPage(View):
     def get(self, request, *args, **kwargs):
         return render(request,"index.html",{'form' : 1234567})
 
-    ##def post(self, request, *args,**kwargs):
 
+    ##def post(self, request, *args,**kwargs):
