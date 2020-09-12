36a937862c2062eaf5c2f002a7dc21bf6478913c
student <yapkaymen@gmail.com>
2020-09-13 03:11:37 +0800

    Updated setup,sh to auto-yes prompt messages

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sudo add-apt-repository ppa:deadsnakes/ppa
+sudo add-apt-repository ppa:deadsnakes/ppa -y
 sudo apt-get update
-sudo apt-get install python3.5 python3-pip python3-virtualenv
+sudo apt-get install python3.5 python3-pip python3-virtualenv -y
 virtualenv -p /usr/bin/python3.5 ccsep-env
 source ccsep-env/bin/activate
 pip install -r vuln_requirements.txt
