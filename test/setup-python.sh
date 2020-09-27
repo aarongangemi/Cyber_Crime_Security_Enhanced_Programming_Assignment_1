f41d47f002cdef52e8279cf9a4ddd99a67072f35
student <yapkaymen@gmail.com>
2020-09-28 00:07:01 +0800

    Updated Readme.adoc and python setup script

--- test/setup-python.sh
+++ test/setup-python.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-sudo add-apt-repository ppa:deadsnakes/ppa -y
 sudo apt-get update
-sudo apt-get install python3.5 python3-pip python3-virtualenv -y
-virtualenv -p /usr/bin/python3.5 ccsep-env
+sudo apt-get install python3 python3-pip python3-venv -y
+python3 -m venv ccsep-env
 source ccsep-env/bin/activate
-pip install -r vuln_requirements.txt
+pip install -r test_requirements.txt
 deactivate
 echo "Activate virtualenv by typing \'source ccsep-env/bin/activate'"
 echo "Deactivate the virtualenv by typing 'deactivate'"
