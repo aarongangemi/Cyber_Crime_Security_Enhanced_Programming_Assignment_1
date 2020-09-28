bf7d2fa40f1a57e0bb7b400849cbb9f4faf6c5fa
Aaron Gangemi <aaron.gangers@gmail.com>
2020-09-28 21:08:15 +0800

    Space trimmer btn fixd

--- test/redos_spacetrimmer_test.py
+++ test/redos_spacetrimmer_test.py
@@ -5,7 +5,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 ## Purpose: To test space trimmer can cause ReDos
-## Contributors: Aaron Gangemi
 ## Bypass register page with valid values
 selenium = webdriver.Firefox()
 selenium.get('http://localhost:8000/')
@@ -16,7 +15,13 @@ username.send_keys("ReDoSTester")
 email.send_keys("a.a@a.com")
 submit.send_keys(Keys.RETURN)
 timeout = 5
-## Bypass regex page by clicking space trimmer button
+## Bypass regex page with invalid input
+try:
+    element_present = EC.presence_of_element_located((By.ID, 'id_regexString'))
+    WebDriverWait(selenium, timeout).until(element_present)
+except TimeoutException:
+    print("Timed out waiting for page to load")
+
 goToSpace = selenium.find_element_by_id('goToSpaceBtn')
 goToSpace.send_keys(Keys.RETURN)
 
@@ -26,27 +31,25 @@ try:
 except TimeoutException:
     print("Timed out waiting for page to load")
 
-# Testing space trimmer ReDos
+# Testing matching regex and input string
 spaceInput = selenium.find_element_by_id('id_spaceInput')
 trimBtn = selenium.find_element_by_id('trimBtn')
 spaceInput.send_keys("asd")
-## will take time to buffer - approximatety 1 minute
 spaceInput.send_keys(" " * 100000)
+## take time to buffer
 spaceInput.send_keys("asd")
 trimBtn.send_keys(Keys.RETURN)
-## send data
 
 try:
     timeout = 10
+    ## check space result
     text_present = EC.text_to_be_present_in_element((By.ID, 'spaceResult'), "asd")
     WebDriverWait(selenium, timeout).until(text_present)
-    ## if result doesn't appear in 10 seconds, then timeout and ReDos
 except TimeoutException:
     print("ReDoS occurred in space trimmer")
 
 try:
     spaceResult = selenium.find_element_by_id('spaceResult')
-    ## if space result shows asd then no redos, if assertion error then redos
     assert "asd" in spaceResult.text
     print("No ReDos Occurred")
 except AssertionError:
